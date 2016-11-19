2cdd251d40502cd7ad6c0a1efcdaeca5e0a55d27
Yihui Xiong <yihui_xiong@163.com>
2016-11-19 21:04:03 +0800

    add system update entry

--- js/main.js
+++ js/main.js
@@ -7,214 +7,160 @@ respeaker.home = new Vue({
     data: {
         token: '',
         ws: '',
+        //wsUrl: "ws://"+location.host+"/websocket/",
+        wsUrl: "ws://192.168.100.1/websocket/",
         ws_token: '',
         ws_res: {},
 
-        // now choose option
-        nowChooseItem: {},
-
-        // state flag
-        isNormalList: true,
         adminLoginState: false,
-        isCheckUpdate: false,               // check update state
+        isCheckUpdate: false, // check update state
 
-        // updata state
-        skipCount: 0,
-        thisVersion: '',
-        latestVersion: '',
-        latestVersionMd5: '',
+        currentVersion: '',
+        lastestVersion: '',
+
         isOpenUpdate: false,
-        isDownLoadingFirmware: false,
+        isShowMessage: true,
+        systemUpdateMessage: 'Checking',
+        isDownloadProgress: false,
+        isRetryDownload: false,
         isDownLoadingFirmwareDone: false,
         isUpgradeCountingDown: false,
-        downloadProgress: '',
+        upgradingHint: 60,
+        downloadMessage: '',
         md5status: '',
         isMD5Pass: false,
         interval: null,
     },
-    ready: function () {
-        var _self = this;
-        //var wsUrl = "ws://"+location.host+"/websocket/";
-        var wsUrl = "ws://192.168.100.1/websocket/";
-        _self.ws = new WebSocket(wsUrl);
+    ready: function() {
+        var self = this;
+        self.ws = new WebSocket(self.wsUrl);
 
         // console.log(this.ws)
-        _self.ws.onopen = function(res){
+        self.ws.onopen = function(message) {
             console.log("connect");
-            _self.ws.send('{"jsonrpc":"2.0","id":0,"method":"challenge","params":[]}')
+            self.ws.send('{"jsonrpc":"2.0","id":0,"method":"challenge","params":[]}')
         };
 
+        self.ws.onmessage = function(message) {
+            var data = JSON.parse(message.data);
+            self.ws_res = message;
 
-        function checkUpgrade() {
-            var delta = _self.getVerCalculation(_self.thisVersion, _self.latestVersion);
+            if (data.id == 0) {
+                self.ws_token = data.result.token;
+                self.tokenSHA(self.ws_token, 'root');
+            } else if (data.id == 1) {
+                // if login failed
+                if (data.error && data.error.code == 'EACCESS') {
+                    console.log('the token is:' + self.ws_token);
+                    return false;
+                }
 
-            // thisVersion     =  'v0.9.98';
-            // latestVersion   =  'v1.0.08';
+                self.token = data.result.success;
 
-            console.log('Current version:' + _self.thisVersion)
-            console.log('Latest version:' + _self.latestVersion)
+                console.log('Get system information');
+                self.ws.send('{"jsonrpc":"2.0","id":12,"method":"call","params":["' + self.token + '","/system","board",{}]}');
 
-            console.log('Skip ' + _self.skipCount + ' version(s)')
-            console.log(delta)
+                console.log('Check update');
+                self.ws.send('{"jsonrpc":"2.0","id":13,"method":"call","params":["' + self.token + '","/system","check",{}]}');
 
-            if (delta > Number(_self.skipCount) ) {
-                console.log('Update available');
-                _self.isOpenUpdate = true;
-                return true;
-            } else{
-                console.log('No update');
+                adminLogin(data);
+            } else if (data.id == 12) {
+                // Get system information
+                self.currentVersion = data.result.release.revision;
+
+                console.log('Current system version:' + self.currentVersion);
+            } else if (data.id == 13) {
+                // Check update
+                if (data.result.results[1].status == 'skiped' || data.result.results[1].status == 'error' || data.result.results[1].status == 'checking') {
+                    self.systemUpdateMessage = "Current version " + data.result.results[1].current_version + " is the latest.";
+                    return;
+                } else if (data.result.results[1].status == 'downloading') {
+                    console.log('Downloading firmware');
+                    self.interval = setInterval(function() {
+                        // get progress
+                        self.ws.send('{"jsonrpc":"2.0","id":15,"method":"call","params":["' + self.token + '","/system","progress",{}]}');
+                    }, 1000);
+
+                    self.isShowMessage = false;
+                    self.downloadMessage = 'Checking ...';
+                    self.isDownloadProgress = true;
+                    self.isOpenUpdate = true;
+                } else if (data.result.results[1].status == 'ok') {
+                    self.currentVersion = data.result.results[1].current_version;
+                    self.lastestVersion = data.result.results[0].lastest_version;
+
+                    self.isShowMessage = false;
+                    self.isOpenUpdate = true;
+                }
+
+            } else if (data.id == 14) {
+                if (JSON.parse(message.data).result.results == 'done') {
+                    console.log('Download started');
+                    self.interval = setInterval(function() {
+                        // get progress
+                        self.ws.send('{"jsonrpc":"2.0","id":15,"method":"call","params":["' + self.token + '","/system","progress",{}]}');
+                    }, 1000);
+                } else {
+
+                }
+            } else if (data.id == 15) {
+                // Download progress
+                if (data.hasOwnProperty('result')) {
+                    if (data.result.results[0].status == 'downloading') {
+                        self.downloadMessage = 'Downloading ' + data.result.results[0].progress + '%';
+                        console.log('progress:' + self.downloadMessage)
+                    } else if (data.result.results[0].status == 'ok') {
+                        self.downloadMessage = 'Downloaded ' + data.result.results[0].progress + '%';
+                        clearInterval(self.interval);
+                        self.isOpenUpdate = true;
+                        self.isDownloadProgress = false;
+                        self.isDownLoadingFirmwareDone = true;
+                    } else {
+                        self.downloadMessage = 'Downloaded ' + data.result.results[0].progress + '%, but failed!';
+                        clearInterval(self.interval);
+                        self.isRetryDownload = true;
+                    }
+                } else {
+                    clearInterval(self.interval);
+                    self.downloadMessage = 'Unkown';
+                }
+
+            } else if (data.id == 16) {
+                // Start upgrading
+                self.isDownloadProgress = false;
+                self.isDownLoadingFirmwareDone = false;
+                self.isUpgradeCountingDown = true;
+
+                self.upgradingHint = 60
+                self.interval = setInterval(function() {
+                    self.upgradingHint -= 1;
+                    if (self.upgradingHint == 1) {
+                        clearInterval(self.interval);
+                        location.reload();
+                    }
+                }, 1000);
             }
-
-            return false;
         }
 
+        self.ws.onerror = function(message) {
+            console.log(message);
+        }
 
+        self.ws.onclose = function(message) {
+            console.log(message);
+        }
 
-        _self.ws.onmessage = function(res){
-
-            _self.ws_res = res;
-
-                if (JSON.parse(res.data).id == 0){
-                    _self.ws_token = JSON.parse(res.data).result.token;
-                    _self.tokenSHA(_self.ws_token, 'root');
-                }else if (JSON.parse(res.data).id == 1){
-
-                    // if admin login
-                    if(JSON.parse(res.data).error && JSON.parse(res.data).error.code == 'EACCESS') {
-                        isNormalList = !isNormalList;
-                        console.log('the token is:'+ws_token);
-                        return false;
-                    }
-
-
-                    // from admin login
-                    if(_self.adminLoginState == true) {
-                        _self.isNormalList = true;
-                    }
-
-                    _self.token = JSON.parse(res.data).result.success;
-
-
-                   _self.ws.send('{"jsonrpc":"2.0","id":12,"method":"call","params":["'+_self.token+'","/system","board",{}]}');
-
-                   _self.ws.send('{"jsonrpc":"2.0","id":18,"method":"call","params":["'+_self.token+'","/system","getskip",{}]}');
-
-                    console.log('Check update');
-
-                    $.ajax({
-                            //url: 'https://s3-us-west-2.amazonaws.com/respeaker.io/firmware/version.json',
-                            url: 'http://192.168.4.48:8000/version.json',
-                            timeout: 9000,
-                            dataType: 'jsonp',
-                            jsonpCallback: 'checkVersion',
-                            success: function(data) {
-                                console.log(data);
-                                _self.latestVersion  = data.result.version;
-                                _self.latestVersionMd5 = data.result.md5sum;
-                                console.log('Latest version:' + _self.latestVersion);
-
-                                checkUpgrade();
-                            },
-                        });
-
-                    adminLogin(res);
-                }
-
-
-                if(JSON.parse(res.data).id == 12) {
-                    // Get current version
-                    _self.thisVersion = JSON.parse(res.data).result.release.revision;
-
-                    console.log('Current system version:' + _self.thisVersion);
-                }
-
-                // 14 get progress
-                if(JSON.parse(res.data).id == 14 ) {
-                    console.log(res.data);
-                    response = JSON.parse(res.data);
-                    if (response.hasOwnProperty('result')) {
-                        _self.downloadProgress = response.result.results[0].size;
-                        console.log('progress:' + _self.downloadProgress)
-                        if (_self.downloadProgress == '100%') {
-                            clearInterval(_self.interval);
-
-                            _self.isDownLoadingFirmware = false;
-                            var check_md5_param = '{"jsonrpc": "2.0","id": 16,"method":"call","params": ["'+_self.token+'","/system","check",{}] }';
-                           _self.ws.send(check_md5_param);
-                           _self.md5status = 'Checking MD5...';
-                            _self.isDownLoadingFirmwareDone = true;
-                        }
-                    }
-
-                }
-
-                // 15 download
-                if(JSON.parse(res.data).id == 15 ) {
-                    if( JSON.parse(res.data).result.results == 'done' ) {
-                        console.log('Download started');
-                        _self.interval = setInterval(function() {
-                            // get progress
-                            var get_progress_param = '{"jsonrpc":"2.0","id":14,"method":"call","params":["'+_self.token+'","/system","progress",{}]}';
-                            _self.ws.send(get_progress_param);
-                        }, 1000);
-                    } else {
-
-                    }
-                }
-
-                // 16 check md5
-                if(JSON.parse(res.data).id == 16 ) {
-                    // console.log(JSON.parse(res.data).result.results)
-                    var download_md5  = JSON.parse(res.data).result.results;
-                    console.log(download_md5);
-                    console.log(_self.latestVersionMd5);
-                    if( download_md5 == _self.latestVersionMd5 ){
-                        _self.isMD5Pass = true;
-                    } else {
-                        _self.md5status = 'MD5 not match';
-                    }
-                }
-
-                // 17 update button clicked
-                if(JSON.parse(res.data).id == 17 ) {
-                    // console.log(JSON.parse(res.data).result.results)
-                    _self.isDownLoadingFirmware = false;
-                    _self.isDownLoadingFirmwareDone = false;
-
-                    _self.isUpgradeCountingDown = true;
-                }
-
-                // 18 check skip
-                if(JSON.parse(res.data).id == 18){
-                    _self.skipCount = JSON.parse(res.data).result.results;
-                    console.log('skipCount is '+_self.skipCount);
-
-
-                }
-
-
-
+        function adminLogin(data) {
+            var token = data.result.success;
+            if (token != null) {
+                self.ws.send('{"jsonrpc": "2.0","id": 3,"method": "call","params": ["' + token + '","/juci/rewifi","scan",{"device": "ra0"}]}');
             }
-
-            this.ws.onerror = function(res){
-                console.log(res);
-            }
-
-            this.ws.onclose = function(res){
-                console.log(res);
-            }
-
-            function adminLogin(res){
-                var token = JSON.parse(res.data).result.success;
-                if(token != null){
-                    _self.ws.send('{"jsonrpc": "2.0","id": 3,"method": "call","params": ["'+token+'","/juci/rewifi","scan",{"device": "ra0"}]}');
-                }
-            }
+        }
     },
     methods: {
-        tokenSHA: function (the_token, psw){
+        tokenSHA: function(the_token, psw) {
             console.log(the_token)
-            // var token = JSON.parse(res.data).result.token;
+                // var token = JSON.parse(message.data).result.token;
             var token = the_token;
             var sha = new jsSHA("SHA-1", "TEXT");
             var pwhash = new jsSHA("SHA-1", "TEXT");
@@ -227,40 +173,54 @@ respeaker.home = new Vue({
             var str = sha.getHash("HEX");
             // this.token =sha.getHash("HEX");
 
-            console.log("admin login");
-            this.ws.send('{"jsonrpc":"2.0","id":1,"method":"login","params":["admin","'+ str +'"]}');
+            console.log("login");
+            this.ws.send('{"jsonrpc":"2.0","id":1,"method":"login","params":["admin","' + str + '"]}');
+        },
+
+        checkSystemUpdate: function() {
+            // update version
+            this.isShowMessage = true;
+            this.isDownloadProgress = false;
+            this.isRetryDownload = false;
+            this.isDownLoadingFirmwareDone = false;
+            this.systemUpdateMessage = "Checking ...";
+            this.isOpenUpdate = true;
+            this.ws.send('{"jsonrpc":"2.0","id":13,"method":"call","params":["' + this.token + '","/system","check",{}]}');
+        },
+
+        hideSystemUpdate: function() {
+            // update version
+            this.isRetryDownload = false;
+            this.isOpenUpdate = false;
+        },
+
+        updateFirmware: function() {
+            // update version
+            this.isRetryDownload = false;
+            this.isDownloadProgress = true;
+            this.ws.send('{"jsonrpc": "2.0","id": 14,"method":"call","params": ["' + this.token + '","/system","download",{}] }');
         },
 
         // get version differences count
-        getVerCalculation: function (ver_now, ver_new) {
+        getVerCalculation: function(ver_now, ver_new) {
             var count_1 = ver_now.substr(1).replace(/\./g, ''),
                 count_2 = ver_new.substr(1).replace(/\./g, '');
-                return (Number(count_2) - Number(count_1));
+            return (Number(count_2) - Number(count_1));
         },
 
-        updateFirmware: function(){
-            // console.log(this.ws_res);
-            // console.log(this.ws);
-
-            // update version
-            this.isDownLoadingFirmware = true;
-            var dl_param  = '{"jsonrpc": "2.0","id": 15,"method":"call","params": ["'+this.token+'","/system","download",{}] }';
-            this.ws.send(dl_param);
-        },
-
-        skipFirmware: function(){
+        skipFirmware: function() {
             console.log('skip this version ');
             this.isOpenUpdate = false;
-            var count = this.getVerCalculation(this.thisVersion, this.latestVersion),
-            skip_param = '{"jsonrpc":"2.0","id":12,"method":"call","params":["'+this.token+'","/system","setskip",{"value":"'+count+'"}]}';
+            var count = this.getVerCalculation(this.currentVersion, this.latestVersion),
+                skip_param = '{"jsonrpc":"2.0","id":12,"method":"call","params":["' + this.token + '","/system","setskip",{"value":"' + count + '"}]}';
             console.log(count);
             // console.log(skip_param);
             this.ws.send(skip_param);
             // skip version done
         },
 
-        updateNow: function(){
-            var final_update_param = '{"jsonrpc":"2.0","id":17,"method":"call","params":["'+this.token+'","/system","upgrade",{}]}';
+        updateNow: function() {
+            var final_update_param = '{"jsonrpc":"2.0","id":16,"method":"call","params":["' + this.token + '","/system","upgrade",{}]}';
             this.ws.send(final_update_param);
         }
     }
